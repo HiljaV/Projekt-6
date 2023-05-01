9aab2e4ffd79a867271d55d84a6aa4f24c074b04
Hilja Van <hilja.van@studerande.karlskogafhs.se>
2023-05-01 19:48:46 +0200

    Add Javascript on header scroll

---
+++ src/js/headerScroll.js
@@ -0,0 +1,26 @@
+const headerScroll = () => {
+
+    //Get elements from DOM
+    const header = document.querySelector('.site-header')
+
+    // Toggle Header Class
+    const toggleHeaderClass = () => {
+        
+        // If window has scrolled more than 200px, add a class
+        // Else remove class
+        if ( window.scrollY > 200 ) {
+
+            header.classList.add('site-header--scroll')
+
+        }
+
+        else {
+            header.classList.remove('site-header--scroll')
+        }
+    }
+
+    //Add event to window scroll
+    window.addEventListener('scroll', toggleHeaderClass)
+}
+
+headerScroll()
